f1f92cf7b79a14748307bb7ed3d69b1cfb97d12b
Alina Ovod <alinaovod.work@gmail.com>
2026-08-27 19:24:54 +0300

    feat: integrate Prisma for database management and set up initial schema

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+/* eslint-disable no-console */
+
 'use strict';
+
+import { createServer } from './app';
+
+const PORT = process.env.PORT || 3007;
+
+createServer().listen(PORT, () => {
+  console.log(`Server is running on localhost:${PORT}`);
+});
